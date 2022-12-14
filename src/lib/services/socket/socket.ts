96583e9c2c5cc472178972abc8e61915c775781e
Dwynr <dwynr@outlook.de>
2022-12-14 02:04:53 +0100

    fix: reconnect socket

--- src/lib/services/socket/socket.ts
+++ src/lib/services/socket/socket.ts
@@ -171,142 +171,154 @@ export type SocketEvent = {
 
 let CONNECTED: boolean = false
 let PING_INTERVAL: any = undefined
+let SOCKET_HANDLE: any = undefined
 
-const socket = io(SOCKET, {
-    path: "",
-    reconnect: true,
-    reconnection: true,
-    transports: [
-        "websocket"
-    ]
-})
-
-socket.on("connect", async () => {
-    CONNECTED = true
-
-    console.log("Connected to socket server")
-
-    const loggedIn = cookies.get("loggedIn")
-
-    if(loggedIn == "true"){
-        socket.emit("auth", {
-            apiKey: await db.get("apiKey") || ""
-        })
-    
-        clearInterval(PING_INTERVAL)
-    
-        PING_INTERVAL = setInterval(() => {
-            socket.emit("heartbeat")
-        }, 5000)
-    }
-})
-
-socket.on("disconnect", () => {
+export const connect = () => {
     CONNECTED = false
+    SOCKET_HANDLE = undefined
 
-    console.log("Disconnected from socket server")
-})
+    clearInterval(PING_INTERVAL)
 
-socket.on("new-event", (data: SocketNewEvent) => {
-    eventListener.emit("socketEvent", {
-        type: "newEvent",
-        data
-    } as SocketEvent)
-})
+    SOCKET_HANDLE = io(SOCKET, {
+        path: "",
+        reconnect: true,
+        reconnection: true,
+        transports: [
+            "websocket"
+        ]
+    })
+    
+    SOCKET_HANDLE.on("connect", async () => {
+        CONNECTED = true
+    
+        console.log("Connected to socket server")
+    
+        const loggedIn = cookies.get("loggedIn")
+    
+        if(loggedIn == "true"){
+            SOCKET_HANDLE.emit("auth", {
+                apiKey: await db.get("apiKey") || ""
+            })
+        
+            clearInterval(PING_INTERVAL)
+        
+            PING_INTERVAL = setInterval(() => {
+                SOCKET_HANDLE.emit("heartbeat")
+            }, 5000)
+        }
+    })
+    
+    SOCKET_HANDLE.on("disconnect", () => {
+        CONNECTED = false
+    
+        console.log("Disconnected from socket server")
 
-socket.on("file-rename", (data: SocketFileRename) => {
-    eventListener.emit("socketEvent", {
-        type: "fileRename",
-        data
-    } as SocketEvent)
-})
+        setTimeout(connect, 5000)
+    })
+    
+    SOCKET_HANDLE.on("new-event", (data: SocketNewEvent) => {
+        eventListener.emit("socketEvent", {
+            type: "newEvent",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-rename", (data: SocketFileRename) => {
+        eventListener.emit("socketEvent", {
+            type: "fileRename",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-archive-restored", (data: SocketFileArchiveRestored) => {
+        eventListener.emit("socketEvent", {
+            type: "fileArchiveRestored",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-new", (data: SocketFileNew) => {
+        eventListener.emit("socketEvent", {
+            type: "fileNew",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-new", (data: SocketFileNew) => {
+        eventListener.emit("socketEvent", {
+            type: "fileNew",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-move", (data: SocketFileMove) => {
+        eventListener.emit("socketEvent", {
+            type: "fileMove",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-trash", (data: SocketFileTrash) => {
+        eventListener.emit("socketEvent", {
+            type: "fileTrash",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("file-archived", (data: SocketFileArchived) => {
+        eventListener.emit("socketEvent", {
+            type: "fileArchived",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-rename", async (data: SocketFolderRename) => {
+        eventListener.emit("socketEvent", {
+            type: "folderRename",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-trash", (data: SocketFolderTrash) => {
+        eventListener.emit("socketEvent", {
+            type: "folderTrash",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-move", async (data: SocketFolderMove) => {
+        eventListener.emit("socketEvent", {
+            type: "folderMove",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-sub-created", async (data: SocketFolderSubCreated) => {
+        eventListener.emit("socketEvent", {
+            type: "folderSubCreated",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-restore", async (data: SocketFolderRestore) => {
+        eventListener.emit("socketEvent", {
+            type: "folderRestore",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("folder-color-changed", (data: SocketFolderColorChanged) => {
+        eventListener.emit("socketEvent", {
+            type: "folderColorChanged",
+            data
+        } as SocketEvent)
+    })
+    
+    SOCKET_HANDLE.on("trash-empty", () => {
+        eventListener.emit("socketEvent", {
+            type: "trashEmpty"
+        } as SocketEvent)
+    })
+}
 
-socket.on("file-archive-restored", (data: SocketFileArchiveRestored) => {
-    eventListener.emit("socketEvent", {
-        type: "fileArchiveRestored",
-        data
-    } as SocketEvent)
-})
-
-socket.on("file-new", (data: SocketFileNew) => {
-    eventListener.emit("socketEvent", {
-        type: "fileNew",
-        data
-    } as SocketEvent)
-})
-
-socket.on("file-new", (data: SocketFileNew) => {
-    eventListener.emit("socketEvent", {
-        type: "fileNew",
-        data
-    } as SocketEvent)
-})
-
-socket.on("file-move", (data: SocketFileMove) => {
-    eventListener.emit("socketEvent", {
-        type: "fileMove",
-        data
-    } as SocketEvent)
-})
-
-socket.on("file-trash", (data: SocketFileTrash) => {
-    eventListener.emit("socketEvent", {
-        type: "fileTrash",
-        data
-    } as SocketEvent)
-})
-
-socket.on("file-archived", (data: SocketFileArchived) => {
-    eventListener.emit("socketEvent", {
-        type: "fileArchived",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-rename", async (data: SocketFolderRename) => {
-    eventListener.emit("socketEvent", {
-        type: "folderRename",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-trash", (data: SocketFolderTrash) => {
-    eventListener.emit("socketEvent", {
-        type: "folderTrash",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-move", async (data: SocketFolderMove) => {
-    eventListener.emit("socketEvent", {
-        type: "folderMove",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-sub-created", async (data: SocketFolderSubCreated) => {
-    eventListener.emit("socketEvent", {
-        type: "folderSubCreated",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-restore", async (data: SocketFolderRestore) => {
-    eventListener.emit("socketEvent", {
-        type: "folderRestore",
-        data
-    } as SocketEvent)
-})
-
-socket.on("folder-color-changed", (data: SocketFolderColorChanged) => {
-    eventListener.emit("socketEvent", {
-        type: "folderColorChanged",
-        data
-    } as SocketEvent)
-})
-
-socket.on("trash-empty", () => {
-    eventListener.emit("socketEvent", {
-        type: "trashEmpty"
-    } as SocketEvent)
-})
+connect()
