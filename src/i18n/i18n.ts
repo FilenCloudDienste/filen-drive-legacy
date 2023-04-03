77427af1e43572e341e76487074275a4f6cb60bf
Dwynr <dwynr@outlook.de>
2023-04-03 12:22:34 +0200

    remove useless memoization

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,5 +1,3 @@
-import { memoize, values } from "lodash"
-
 import en from "./lang/en"
 import de from "./lang/de"
 import ja from "./lang/ja"
@@ -13,59 +11,63 @@ import ko from "./lang/ko"
 import tr from "./lang/tr"
 
 const translations: {
-    [key: string]: {
-        [key: string]: string
-    }
+	[key: string]: {
+		[key: string]: string
+	}
 } = {
-    en,
-    de,
-    ja,
-    pl,
-    es,
-    uk,
-    ru,
-    fr,
-    zh,
-    ko,
-    tr
+	en,
+	de,
+	ja,
+	pl,
+	es,
+	uk,
+	ru,
+	fr,
+	zh,
+	ko,
+	tr
 }
 
-export const i18n = memoize((lang: string = "en", text: string, firstUpperCase: boolean = true, replaceFrom: string[] = [], replaceTo: string[] = []) => {
-    if(typeof lang !== "string"){
-        lang = "en"
-    }
-    
-    let gotText = translations[lang][text]
+export const i18n = (
+	lang: string = "en",
+	text: string,
+	firstUpperCase: boolean = true,
+	replaceFrom: string[] = [],
+	replaceTo: string[] = []
+) => {
+	if (typeof lang !== "string") {
+		lang = "en"
+	}
 
-    if(!gotText){
-        if(translations['en'][text]){
-            gotText = translations['en'][text]
-        }
-        else{
-            console.warn("NO_TRANSLATION_FOUND_" + lang.toString() + "_" + text.toString())
-            
-            return "NO_TRANSLATION_FOUND_" + lang.toString() + "_" + text.toString()
-        }
-    }
+	let gotText = translations[lang][text]
 
-    gotText = gotText.trim()
+	if (!gotText) {
+		if (translations["en"][text]) {
+			gotText = translations["en"][text]
+		} else {
+			console.warn("NO_TRANSLATION_FOUND_" + lang.toString() + "_" + text.toString())
 
-    if(firstUpperCase){
-        gotText = gotText.charAt(0).toUpperCase() + gotText.slice(1)
-    }
-    else{
-        gotText = gotText.charAt(0).toLowerCase() + gotText.slice(1)
-    }
+			return "NO_TRANSLATION_FOUND_" + lang.toString() + "_" + text.toString()
+		}
+	}
 
-    if(replaceFrom.length > 0 && replaceTo.length > 0){
-        for(let i = 0; i < replaceFrom.length; i++){
-            gotText = gotText.split(replaceFrom[i]).join(replaceTo[i])
-        }
-    }
+	gotText = gotText.trim()
 
-    return gotText
-}, (...args) => values(args))
+	if (firstUpperCase) {
+		gotText = gotText.charAt(0).toUpperCase() + gotText.slice(1)
+	} else {
+		gotText = gotText.charAt(0).toLowerCase() + gotText.slice(1)
+	}
 
-export const isLanguageAvailable = memoize((lang: string = "en") => {
-    return typeof translations[lang] == "undefined" ? false : true
-})
+	if (replaceFrom.length > 0 && replaceTo.length > 0) {
+		for (let i = 0; i < replaceFrom.length; i++) {
+			gotText = gotText.split(replaceFrom[i]).join(replaceTo[i])
+		}
+	}
+
+	return gotText
+}
+
+export const isLanguageAvailable = (lang: string = "en") => {
+	return typeof translations[lang] == "undefined" ? false : true
+}
