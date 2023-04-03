a35a3794022bc38a7eb4748d63dfa0ad2be0a06b
Dwynr <dwynr@outlook.de>
2023-04-03 13:09:24 +0200

    linter

--- src/lib/worker/worker.com.ts
+++ src/lib/worker/worker.com.ts
@@ -11,61 +11,84 @@ const workerInstances: Worker[] = []
 const workerAPIs: typeof api[] = []
 
 const getWorkerAPI = () => {
-    if(workerAPIs.length < WORKER_THREADS){
-        const workerInstance = new Worker(new URL("./worker.worker", import.meta.url))
-        const workerAPI = wrap<typeof api>(workerInstance)
+	if (workerAPIs.length < WORKER_THREADS) {
+		const workerInstance = new Worker(new URL("./worker.worker", import.meta.url))
+		const workerAPI = wrap<typeof api>(workerInstance)
 
-        workerInstance.onmessage = (message) => {
-            if(typeof message.data?.type == "string"){
-                if(message.data.type == "uploadProgress"){
-                    eventListener.emit("uploadProgress", message.data)
-                }
-        
-                if(message.data.type == "downloadProgress"){
-                    eventListener.emit("downloadProgress", message.data)
-                }
-            }
-        }
+		workerInstance.onmessage = message => {
+			if (typeof message.data?.type == "string") {
+				if (message.data.type === "uploadProgress") {
+					eventListener.emit("uploadProgress", message.data)
+				}
 
-        workerInstances.push(workerInstance)
-        workerAPIs.push(workerAPI)
-    }
+				if (message.data.type === "downloadProgress") {
+					eventListener.emit("downloadProgress", message.data)
+				}
+			}
+		}
 
-    nextWorkerId += 1
+		workerInstances.push(workerInstance)
+		workerAPIs.push(workerAPI)
+	}
 
-    if(nextWorkerId >= workerAPIs.length){
-        nextWorkerId = 0
-    }
+	nextWorkerId += 1
 
-    return workerAPIs[nextWorkerId]
+	if (nextWorkerId >= workerAPIs.length) {
+		nextWorkerId = 0
+	}
+
+	return workerAPIs[nextWorkerId]
 }
 
-export const apiRequest = async ({ method = "POST", endpoint, data }: { method: string, endpoint: string, data: any }): Promise<any> => {
-    const response = await getWorkerAPI().apiRequest(method, endpoint, data)
+export const apiRequest = async ({
+	method = "POST",
+	endpoint,
+	data
+}: {
+	method: string
+	endpoint: string
+	data: any
+}): Promise<any> => {
+	const response = await getWorkerAPI().apiRequest(method, endpoint, data)
 
-    if(typeof response == "object"){
-        if(typeof response.message == "string"){
-            if(response.message.toLowerCase().indexOf("api key not found") !== -1 || response.message.toLowerCase().indexOf("invalid api key") !== -1){
-                logout()
+	if (typeof response == "object") {
+		if (typeof response.message == "string") {
+			if (
+				response.message.toLowerCase().indexOf("api key not found") !== -1 ||
+				response.message.toLowerCase().indexOf("invalid api key") !== -1
+			) {
+				logout()
 
-                throw new Error("Session invalidated")
-            }
-        }
-    }
+				throw new Error("Session invalidated")
+			}
+		}
+	}
 
-    return response
+	return response
 }
 
-export const generatePasswordAndMasterKeysBasedOnAuthVersion = async (rawPassword: string, authVersion: number, salt: string): Promise<{ derivedMasterKeys: string, derivedPassword: string }> => {
-    return await getWorkerAPI().generatePasswordAndMasterKeysBasedOnAuthVersion(rawPassword, authVersion, salt)
+export const generatePasswordAndMasterKeysBasedOnAuthVersion = async (
+	rawPassword: string,
+	authVersion: number,
+	salt: string
+): Promise<{ derivedMasterKeys: string; derivedPassword: string }> => {
+	return await getWorkerAPI().generatePasswordAndMasterKeysBasedOnAuthVersion(rawPassword, authVersion, salt)
 }
 
-export const deriveKeyFromPassword = async (password: string, salt: string, iterations: number, hash: string, bitLength: number, returnHex: boolean): Promise<string | ArrayBuffer> => {
+export const deriveKeyFromPassword = async (
+	password: string,
+	salt: string,
+	iterations: number,
+	hash: string,
+	bitLength: number,
+	returnHex: boolean
+): Promise<string | ArrayBuffer> => {
 	return await getWorkerAPI().deriveKeyFromPassword(password, salt, iterations, hash, bitLength, returnHex)
 }
 
-export const hashPassword = async (password: string,): Promise<string> => { //old and deprecated, no longer in use
-    return await getWorkerAPI().hashPassword(password)
+export const hashPassword = async (password: string): Promise<string> => {
+	//old and deprecated, no longer in use
+	return await getWorkerAPI().hashPassword(password)
 }
 
 export const hashFn = async (str: string): Promise<string> => {
@@ -80,11 +103,14 @@ export const decryptFolderName = async (metadata: string, masterKeys: string[]):
 	return await getWorkerAPI().decryptFolderName(metadata, masterKeys)
 }
 
-export const decryptFileMetadata = async (metadata: string, masterKeys: string[]): Promise<{ name: string, size: number, mime: string, key: string, lastModified: number }> => {
+export const decryptFileMetadata = async (
+	metadata: string,
+	masterKeys: string[]
+): Promise<{ name: string; size: number; mime: string; key: string; lastModified: number }> => {
 	return await getWorkerAPI().decryptFileMetadata(metadata, masterKeys)
 }
 
-export const generateKeypair = async (): Promise<{ publicKey: string, privateKey: string }> => {
+export const generateKeypair = async (): Promise<{ publicKey: string; privateKey: string }> => {
 	return await getWorkerAPI().generateKeypair()
 }
 
@@ -109,8 +135,22 @@ export const encryptData = async (data: Uint8Array, key: string): Promise<string
 	return await getWorkerAPI().encryptData(transfer(data, [data.buffer]), key)
 }
 
-export const encryptAndUploadFileChunk = async (chunk: Uint8Array, key: string, url: string, uuid: string, chunkIndex: number, chunkSize: number): Promise<any> => {
-	return await getWorkerAPI().encryptAndUploadFileChunk(transfer(chunk, [chunk.buffer]), key, url, uuid, chunkIndex, chunkSize)
+export const encryptAndUploadFileChunk = async (
+	chunk: Uint8Array,
+	key: string,
+	url: string,
+	uuid: string,
+	chunkIndex: number,
+	chunkSize: number
+): Promise<any> => {
+	return await getWorkerAPI().encryptAndUploadFileChunk(
+		transfer(chunk, [chunk.buffer]),
+		key,
+		url,
+		uuid,
+		chunkIndex,
+		chunkSize
+	)
 }
 
 export const encryptMetadataPublicKey = async (data: string, publicKey: string): Promise<string> => {
@@ -134,17 +174,20 @@ export const downloadAndDecryptChunk = async (item: ItemProps, url: string): Pro
 }
 
 export const decryptFolderNameLink = async (metadata: string, linkKey: string): Promise<string> => {
-    return await getWorkerAPI().decryptFolderNameLink(metadata, linkKey)
+	return await getWorkerAPI().decryptFolderNameLink(metadata, linkKey)
 }
 
 export const decryptFileMetadataLink = async (metadata: string, linkKey: string): Promise<any> => {
-    return await getWorkerAPI().decryptFileMetadataLink(metadata, linkKey)
+	return await getWorkerAPI().decryptFileMetadataLink(metadata, linkKey)
 }
 
 export const convertHeic = async (buffer: Uint8Array, format: "JPEG" | "PNG"): Promise<Uint8Array> => {
 	return await getWorkerAPI().convertHeic(transfer(buffer, [buffer.buffer]), format)
 }
 
-export const bufferToHash = async (buffer: Uint8Array, algorithm: "SHA-1" | "SHA-256" | "SHA-512" | "SHA-384"): Promise<string> => {
-    return await getWorkerAPI().bufferToHash(buffer, algorithm)
+export const bufferToHash = async (
+	buffer: Uint8Array,
+	algorithm: "SHA-1" | "SHA-256" | "SHA-512" | "SHA-384"
+): Promise<string> => {
+	return await getWorkerAPI().bufferToHash(buffer, algorithm)
 }
