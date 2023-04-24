6219296eb42aacaa75de20dc30d595ae9a036640
DodoLeDev <59477313+DodoLeDev@users.noreply.github.com>
2023-04-24 21:15:32 +0200

    Minor bug fixes and improvements
    Bugs fixed in this commit:
     - The name of the file contained a typo
     - The help message printed when we called the program without arguments was empty
     - Increased version number

--- src/i18n/lang/translation_assistant.sh
+++ src/i18n/lang/translation_assistant.sh
@@ -9,7 +9,7 @@
 #
 
 # Version code
-VERSION="0.5"
+VERSION="0.6"
 
 # Valid language codes (based on https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
 ACCEPTED_LANGS=( ab aa af ak sq am ar an hy as av ae ay az bm ba eu be bn bi bs br bg my ca ch ce ny zh cu cv kw co cr hr cs da dv nl dz en eo et ee fo fj fi fr fy ff gd gl lg ka de el kl gn gu ht ha he hz hi ho hu is io ig id ia ie iu ik ga it ja jv kn kr ks kk km ki rw ky kv kg ko kj ku lo la lv li ln lt lu lb mk mg ms ml mt gv mi mr mh mn na nv nd nr ng ne no nb nn ii oc oj or om os pi ps fa pl pt pa qu ro rm rn ru se sm sg sa sc sr sn sd si sk sl so st es su sw ss sv tl ty tg ta tt te th bo ti to ts tn tr tk tw ug uk ur uz ve vi vo wa cy wo xh yi yo za zu )
@@ -411,12 +411,12 @@ function convert_ts_to_po() {
 # ARGS: $@ (optional): Arguments provided to the script
 # OUTS: None
 function main() {
+    script_init "$@"
     if [[ -z "$1" ]]; then
         echo "No parameter provided!"
         script_usage
         exit 1
     fi
-    script_init "$@"
     parse_params "$@"
 }
 
