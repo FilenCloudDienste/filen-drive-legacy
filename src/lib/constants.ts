eda2832db91034b0040f8c6d1e041d81af47d99a
Dwynr <dwynr@outlook.de>
2023-05-08 01:28:34 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -30,16 +30,19 @@ export const ONE_YEAR = 86400000 * 365
 export const AUTH_VERSION = 2
 export const PREVIEW_MAX_SIZE = 1024 * 1024 * 128
 
-export const API_V3_DOMAINS = [
-	"https://gateway.filen.io",
-	"https://gateway.filen.net",
-	"https://gateway.filen-1.net",
-	"https://gateway.filen-2.net",
-	"https://gateway.filen-3.net",
-	"https://gateway.filen-4.net",
-	"https://gateway.filen-5.net",
-	"https://gateway.filen-6.net"
-]
+export const API_V3_DOMAINS =
+	process.env.NODE_ENV === "development"
+		? ["http://localhost:1337"]
+		: [
+				"https://gateway.filen.io",
+				"https://gateway.filen.net",
+				"https://gateway.filen-1.net",
+				"https://gateway.filen-2.net",
+				"https://gateway.filen-3.net",
+				"https://gateway.filen-4.net",
+				"https://gateway.filen-5.net",
+				"https://gateway.filen-6.net"
+		  ]
 
 export const DOWNLOAD_DOMAINS = [
 	"https://down.filen.io",
