21875dfb244a2f18718eb82b51327ddaf5f74079
Dwynr <dwynr@outlook.de>
2023-08-21 02:06:12 +0200

    move emojis to cdn

--- src/components/Chats/customEmojis.ts
+++ src/components/Chats/customEmojis.ts
@@ -3,55 +3,55 @@ export const customEmojis = [
 		id: "kekw",
 		name: "KEKW",
 		keywords: ["filen", "kekw"],
-		skins: [{ src: "./emojis/kekw.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/kekw.webp" }]
 	},
 	{
 		id: "lulw",
 		name: "LULW",
 		keywords: ["filen", "lulw"],
-		skins: [{ src: "./emojis/lulw.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/lulw.webp" }]
 	},
 	{
 		id: "wicked",
 		name: "WICKED",
 		keywords: ["filen", "wicked"],
-		skins: [{ src: "./emojis/wicked.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/wicked.webp" }]
 	},
 	{
 		id: "sadge",
 		name: "Sadge",
 		keywords: ["filen", "sadge"],
-		skins: [{ src: "./emojis/sadge.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/sadge.webp" }]
 	},
 	{
 		id: "catjam",
 		name: "catJam",
 		keywords: ["filen", "catjam"],
-		skins: [{ src: "./emojis/catjam.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/catjam.webp" }]
 	},
 	{
 		id: "gigachad",
 		name: "GIGACHAD",
 		keywords: ["filen", "gigachad"],
-		skins: [{ src: "./emojis/gigachad.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/gigachad.webp" }]
 	},
 	{
 		id: "okayge",
 		name: "Okayge",
 		keywords: ["filen", "okayge"],
-		skins: [{ src: "./emojis/okayge.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/okayge.webp" }]
 	},
 	{
 		id: "pogu",
 		name: "PogU",
 		keywords: ["filen", "pogu"],
-		skins: [{ src: "./emojis/pogu.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/pogu.webp" }]
 	},
 	{
 		id: "pepega",
 		name: "Pepega",
 		keywords: ["filen", "pepega"],
-		skins: [{ src: "./emojis/pepega.webp" }]
+		skins: [{ src: "https://cdn.filen.io/emojis/pepega.webp" }]
 	}
 ]
 
